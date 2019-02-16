0002237a94b960c1921a361988586f67078d7f6e
ArielGad <ariel.gad4@gmail.com>
2019-02-16 21:08:39 +0200

    add index.html add 404 page not found

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const http = require("http");
+const fs = require("fs");
 
 const port = 7000;
 const host = '127.0.0.1';
 
+
+// 404 response
+function send404ErrorMessage(response){
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("Error 404 - Page not found!");
+    response.end();
+}
+
 function handleRequest(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    if ( req.method === 'GET' && req.url === '/'){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        fs.createReadStream('./index.html').pipe(res);
+    }
+    else {
+        send404ErrorMessage(res);
+    }
 
 }
 
